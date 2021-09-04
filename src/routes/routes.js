9a28859914d2d482a631d5460d39f1c95826033f
joshuahiskraken <never.noes.best@gmail.com>
2021-09-04 13:54:58 -0500

    STORMLITE_FETCH rework

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -59,7 +59,7 @@ module.exports = (app) => {
                 //eval(require('locus'))
                 var config = {
                     method: 'get',
-                    url: process.env.FETCH_URL+location || `${process.env.STORMLITE_FETCH}${location}`,
+                    url: process.env.FETCH_URL+location || `${process.env.STORMLITE_FETCH}=${location}`,
                     headers: { }
                   };
 //                   2021-08-31T01:42:34.908451+00:00 app[web.1]: _currentUrl: 'http:undefinedAustin,%20Texas,%20United%20States',
